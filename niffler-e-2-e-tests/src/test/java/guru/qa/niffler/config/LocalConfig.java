3fee11255988365123db439d2e36bc44a6d61f6f
zmarchenko <zhanna.bordakova92@gmail.com>
2024-06-25 20:25:33 +0300

    (lesson 6): project-configuration

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/config/LocalConfig.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/config/LocalConfig.java
@@ -4,24 +4,22 @@ public class LocalConfig implements Config {
 
     static final LocalConfig instance = new LocalConfig();
 
+    private static final String URL = "http://127.0.0.1:";
+
     @Override
-    public String frontUrl() {
-        return "http://127.0.0.1:3000/";
-    }
+    public String frontUrl() {return URL + "3000/";}
 
     @Override
     public String spendUrl() {
-        return "http://127.0.0.1:8093/";
+        return URL + "8093/";
     }
 
     @Override
     public String gatewayUrl() {
-        return "http://127.0.0.1:8090/";
+        return URL + "8090/";
     }
 
     @Override
-    public String dbHost() {
-        return "127.0.0.1";
-    }
+    public String dbHost() {return "127.0.0.1";}
 
 }
